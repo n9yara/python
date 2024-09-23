69a67251e52dc286ebf105556b79865e8302b737
n9yara <zdelucuaffg@gmail.com>
2024-09-23 18:08:27 -0300

    Create ex088.py

---
+++ Exercicios/ex088.py
@@ -0,0 +1,24 @@
+from random import randint as rando
+from time import sleep as timer
+lista = []
+listadois = []
+total = 1
+jogos = int(input('Quantos jogos voce quer que eu sorteie? '))
+while total <= jogos:
+    cont = 0
+    while True:
+        num = rando(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+            if cont >= 6:
+                break
+    lista.sort() 
+    listadois.append(lista[:])   
+    lista.clear()
+    total += 1 
+timer(0.5)
+print(f'SORTEANDO...')
+for i, l in enumerate(listadois):
+    timer(1)
+    print(f'Jogo {i+1}: {l}')
