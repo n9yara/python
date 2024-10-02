eb271d9db58d80433bbe05d94e1a31267c17e244
n9yara <zdelucuaffg@gmail.com>
2024-10-02 12:05:04 -0300

    Create ex096.py

---
+++ Exercicios/ex096.py
@@ -0,0 +1,7 @@
+def area(largura, comprimento):
+    a = largura * comprimento
+    print(f"Com uma area de {largura}X{comprimento} a area vai ser {a}")
+
+lar = float(input("Largura: "))
+comp = float(input("Comprimento: "))
+area(lar, comp)
